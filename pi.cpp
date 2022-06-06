e3e0fba26b24fe2fab90f233543cc36af13e4639
farm24 <77849362+farm24@users.noreply.github.com>
2022-06-05 19:54:03 -0500

    hii

--- pi.cpp
+++ pi.cpp
@@ -1,8 +1,20 @@
-#include "iostream"
-using namespace std;
+#include <cstdio>
+#include "sumtk.hpp"
 
-int main() {
-    for (int x = 0; x <= 1000000; x++) {
-        cout << x << '\n';
-    }
+int main()
+{
+   const std::size_t dsize = 60000007;
+   double* d = new double[dsize];
+
+   for (std::size_t i = 0; i < dsize; ++i)
+   {
+      d[i] = (i & 1) ? 0.0000000123 : 12345.0000000001;
+   }
+
+   double sum = sumtk::sum(d,d + dsize,8);
+
+   printf("sum: %35.15f",sum);
+
+   delete[] d;
+   return 0;
 }
