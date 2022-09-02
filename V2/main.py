22556d80cf96cc063f9b1dcacc2977aa791b96b0
farm24 <77849362+farm24@users.noreply.github.com>
2022-09-02 13:09:09 -0500

    wrote the csv and txt writer code

--- V2/main.py
+++ V2/main.py
@@ -1,3 +1,16 @@
 from mpmath import *
 mp.dps = 100; mp.pretty = True
-fadd(1, 2)
+x = fadd(1, 2)
+with open('main.txt', 'w') as txt:
+    txt.write(x)
+with open('main.csv', 'w') as csv:
+    y = ()
+    z = 1
+    csv.write('Number, Value')
+    for num in str(x):
+        if num = '.':
+            csv.write(f'Decimal, {num}')
+        else:
+            csv.write(f'{z},{num}')
+        csv.flush()
+        z+=1
