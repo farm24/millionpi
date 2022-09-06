0c548d8713f3530efbc6a2a58f96315218f708c2
farm24 <cgb41773@gmail.com>
2022-09-06 16:23:51 -0500

    idk

--- V2/main.py
+++ V2/main.py
@@ -4,17 +4,4 @@ mp.dps = 100000; mp.pretty = True
 x = mpf(1)/mpf(3)
 with open('main.txt', 'w', newline='') as txt:
     txt.write(str(x))
-with open('main.csv', 'w', newline='') as csva:
-    csvr = csv.writer(csva)
-    y = ()
-    z = 1
-    csvr.writerow(['Number', 'Value'])
-    for num in str(x):
-        if num == '.':
-            y=['Decimal', '.']
-            csvr.writerow(y)
-        else:
-            y = [z, num]
-            csvr.writerow(y)
-        z+=1
-        y=[]
+
