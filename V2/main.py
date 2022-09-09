a7b71e8565e6f44162330ae422069a9a1cfe9de6
farm24 <77849362+farm24@users.noreply.github.com>
2022-09-09 13:37:34 -0500

    idk

--- V2/main.py
+++ V2/main.py
@@ -1,6 +1,6 @@
 from mpmath import *
 import csv
 mp.dps = 100; mp.pretty = True
-x = nsum(lambda n:)
+x = (mpf(426880)*sqrt(10005))nsum(lambda n: (()), [0, inf])
 with open('main.txt', 'w', newline='') as txt:
     txt.write(str(x))
