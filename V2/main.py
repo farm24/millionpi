57ebfeaee233a9c4b73a7367f453c0b562236955
farm24 <77849362+farm24@users.noreply.github.com>
2022-09-10 09:41:24 -0500

    idk its broken

--- V2/main.py
+++ V2/main.py
@@ -1,6 +1,6 @@
 from mpmath import *
 import csv
 mp.dps = 100; mp.pretty = True
-x = mpf(426880)*sqrt(10005)*(nsum(lambda q: ((mpf(-1))^(q)*fac((mpf(6)*q))*(mpf(545140134*q+13591409)))/((fac(mpf(3*q)))*(fac(q)^(mpf(3)))*(mpf(-262537412640768000))^(q)), [0, inf]))^mpf(-1)
+x = mpf(426880)*sqrt(10005)*(nsum(lambda q: ((mpf(-1))^(q)*fac((mpf(6)*q))*(mpf(545140134*q+13591409)))/((fac(mpf(3*q)))*(fac(q)^(mpf(3)))*(mpf(-262537412640768000))^(q)), [0, inf]))^(mpf(-1))
 with open('main.txt', 'w', newline='') as txt:
     txt.write(str(x))
