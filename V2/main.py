b473c1e8aee7afafd1cd00c7d2f5e1d394894cae
farm24 <77849362+farm24@users.noreply.github.com>
2022-09-19 10:14:20 -0500

    i think i fixed it

--- V2/main.py
+++ V2/main.py
@@ -1,6 +1,6 @@
 from mpmath import *
 import csv
 mp.dps = 100; mp.pretty = True
-x = mpf()*sqrt()*(nsum(lambda q: (), [0, inf]))**(mpf(-1))
+x = (mpf(426880)*sqrt(10005))*(nsum(lambda q: ((fac((mpf(6)*q)*(mpf(545140134)*q+mpf(13591409))) )/(fac(mpf(3)*q)*(fac(q)**(3)*(-262537412640768000)**(q))) ), [0, inf]))**(mpf(-1))
 with open('main.txt', 'w', newline='') as txt:
-    txt.write(str(x))
+    txt.write(str(x))9
